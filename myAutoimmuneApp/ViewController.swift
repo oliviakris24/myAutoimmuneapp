e009cfdad7f48de5e36c4241ed0f2e7ff193a31a
Olivia Kris <oliviakris@Olivias-MacBook-Air.local>
2019-10-05 14:33:04 -0700

    first commit :)

---
+++ myAutoimmuneApp/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  myAutoimmuneApp
+//
+//  Created by Olivia Kris on 10/3/19.
+//  Copyright © 2019 Olivia Kris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBAction func call911(_ sender: Any) {
+    }
+    @IBAction func tipsAndTricks(_ sender: Any) {
+    }
+    
+    @IBAction func theBasics(_ sender: Any) {
+    }
+    @IBAction func about(_ sender: Any) {
+    }
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
