b285b274a701faf4726d31a8e5de3f6810e4f650
Olivia Kris <oliviakris@Olivias-MacBook-Air.local>
2020-02-04 14:57:09 -0800

    hi :)

--- myAutoimmuneApp/ViewController.swift
+++ myAutoimmuneApp/ViewController.swift
@@ -101,7 +101,6 @@ class ViewController: UIViewController {
     
 
     @IBAction func donateButton(_ sender: Any) {
-        
     }
     override func viewDidLoad() {
         super.viewDidLoad()
