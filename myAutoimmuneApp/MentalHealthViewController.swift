2b3754c038b75f4f7df10e98e9a3657b3c752846
Olivia Kris <oliviakris@Olivias-Air-2.frontierlocal.net>
2020-05-17 19:58:05 -0700

    may 17 updated version

---
+++ myAutoimmuneApp/MentalHealthViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MentalHealthViewController.swift
+//  myAutoimmuneApp
+//
+//  Created by Olivia Kris on 3/10/20.
+//  Copyright © 2020 Olivia Kris. All rights reserved.
+//
+
+import UIKit
+
+class MentalHealthViewController: UIViewController {
+
+    @IBOutlet var button1: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+ 
+    }
+    
+
+}
